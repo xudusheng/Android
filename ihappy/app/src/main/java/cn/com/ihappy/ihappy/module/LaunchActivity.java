ad7c413caef84d2ce563ca3fbc698aaadd876c06
xudusheng <xudusheng@bisinuolan.com>
2018-12-05 18:15:20 +0800

    no message

--- ihappy/app/src/main/java/cn/com/ihappy/ihappy/module/LaunchActivity.java
+++ ihappy/app/src/main/java/cn/com/ihappy/ihappy/module/LaunchActivity.java
@@ -1,4 +1,15 @@
 package cn.com.ihappy.ihappy.module;
 
-public class LaunchActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.com.ihappy.ihappy.R;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
 }
